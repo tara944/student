edbcf3347ee7fab6dda97d91e868e51a53448915
tara944 <koranga134@gmail.com>
2025-12-03 13:11:52 +0530

    deleted unnecssary modules

--- api_sirch/opreations.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-import joson
-response=requests.get("")
--- api_sirch/sirch.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-
-def search_by_id():
-    response=request.get("https://api.sampleapis.com/coffee/hot")
-    prinyt(json.dumps(responce.json(),indent=4))
